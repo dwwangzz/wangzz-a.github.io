600a3d21f1e1116c86d804a790a57aabf80dd246
奈斯曼 <81303012@qq.com>
2018-07-09 15:40:41 +0800

    地址优化

--- vip-video/js/index.js
+++ vip-video/js/index.js
@@ -9,6 +9,9 @@ $(function () {
     $('.naisidamowang').keydown(function (event) {
         if (event.keyCode == 13) {
             var url = $(this).val();
+            if (url.trim().length == 0) {
+                return false;
+            }
             goVideo(url);
         }
     });
